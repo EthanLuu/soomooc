f97b8c6d403b3066af88109a1ba951dd62df786b
ethanol <952792901@qq.com>
2021-05-08 20:39:32 +0800

    feat: 使用 craco 配置主题

--- src/screens/demo.tsx
+++ src/screens/demo.tsx
@@ -1,5 +1,14 @@
 import { LivePlayer } from 'components/live-player'
+import styled from '@emotion/styled'
 
 export const Demo = () => {
-  return <LivePlayer url="http://localhost:7001/live/demo.flv" type="flv" />
+  return (
+    <PlayerContainer>
+      <LivePlayer url="http://localhost:7001/live/demo.flv" type="flv" />
+    </PlayerContainer>
+  )
 }
+
+const PlayerContainer = styled.div`
+  text-align: center;
+`
