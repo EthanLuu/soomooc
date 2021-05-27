82ce417ccb25e824431d99cb192f444d167837e6
ethanol <952792901@qq.com>
2021-05-27 16:07:21 +0800

    feat: 中英文切换功能

---
+++ src/i18n/config.ts
@@ -0,0 +1,27 @@
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+
+import translate_en from './en.json'
+import translate_zh from './zh.json'
+
+const resources = {
+  en: {
+    translation: translate_en,
+  },
+  zh: {
+    translation: translate_zh,
+  },
+}
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources,
+    lng: 'zh', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss
+    },
+  })
+
+export default i18n
