dae6df52ec3c12c11b3917ba404f983b78846528
EthanLuu <ethanlooo@outlook.com>
2024-02-11 16:16:37 +0800

    feat: 更新图标

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+import { pluginSvgr } from '@rsbuild/plugin-svgr'
 
 export default defineConfig({
   plugins: [pluginReact(), pluginSvgr()],
+  html: {
+    title: 'Soo Mooc',
+  },
   source: {
     alias: {
       '@': './src',
-    }
-  }
-});
+    },
+  },
+})
